a0e98abdd8e32ec42b49cfd421124a1f5e01a0e8
elmane27 <104934165+elmane27@users.noreply.github.com>
2022-10-09 10:46:50 -0500

    Update db-connection-mongo.js

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const getConnection = async () => {
     try {
-        const url =
-            "mongodb://user_bd:1IT4n88cqF81QGYH@ac-qbtir1l-shard-00-00.yffhafa.mongodb.net:27017,ac-qbtir1l-shard-00-01.yffhafa.mongodb.net:27017,ac-qbtir1l-shard-00-02.yffhafa.mongodb.net:27017/inventario-youtube?ssl=true&replicaSet=atlas-ych3el-shard-0&authSource=admin&retryWrites=true&w=majority";
+        const url = "mongodb://user_bd:1IT4n88cqF81QGYH@ac-qbtir1l-shard-00-00.yffhafa.mongodb.net:27017,ac-qbtir1l-shard-00-01.yffhafa.mongodb.net:27017,ac-qbtir1l-shard-00-02.yffhafa.mongodb.net:27017/inventario-youtube?ssl=true&replicaSet=atlas-ych3el-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-        await mongoose.connect(url);
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
         console.log("conexion exitosa");
     } catch (error) {
