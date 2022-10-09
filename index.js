17f0f4ea89f181b77e3c8a074bed64472bca3520
elmane27 <104934165+elmane27@users.noreply.github.com>
2022-10-09 10:39:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // origins *
-app.use(cors({
-    origin: '*'    
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 getConnection();
 
